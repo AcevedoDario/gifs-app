2bcf7c0d9d4be1edcba0995f4c68a75fb52b480c
Dario Acevedo <dario_acevedo_1990@hotmail.com>
2022-06-04 13:01:29 -0300

    Added fetch function with default parameters

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import getGifs from "./services/getGifs";
 
-const GIFS = [
-  'https://media1.giphy.com/media/MbAnK3TOrmBLeix0wF/giphy.gif?cid=ecf05e47za2nj2wudulomorgoxteq4auqq3drnpxop3q88n7&rid=giphy.gif&ct=g',
-  'https://media0.giphy.com/media/5Ijmu7ccsOxZL6H77c/giphy.gif?cid=ecf05e47za2nj2wudulomorgoxteq4auqq3drnpxop3q88n7&rid=giphy.gif&ct=g'
-]
-
-const DIFFERENT_GIFS = [
-  'https://media1.giphy.com/media/HIIQJ0Y43ytrGzuP8U/giphy.gif?cid=ecf05e47za2nj2wudulomorgoxteq4auqq3drnpxop3q88n7&rid=giphy.gif&ct=g'
-]
 
 function App() {
 
-  const [gifs, setgifs] = useState(GIFS)
+  const [gifs, setGifs] = useState([])
+  
+  useEffect(function () {
+    getGifs({keyword : 'argentina'}).then(gifs => setGifs(gifs))
+  }, [])
+
 
   return (
     <div className="App">
       <section className="App-content">
         <h1>Gifs App</h1>
-        <br />
-        <br />
         <picture>
-        {gifs.map(gif => (
-          <img src={gif} alt="Gifs" />
-        ))}
+        {
+          gifs.map(simpleGif => <img src={simpleGif} alt="Gif" />)
+        }
         </picture>
         <br />
         <br />
-        <button onClick={() => setgifs(DIFFERENT_GIFS)}>Cambiar gifs </button> 
 
       </section>
     </div>
---
+++ src/services/getGifs.js
@@ -0,0 +1,16 @@
+const apiKey = '2cggEMhx127YydSr9br12cw4V91aIW7K'
+
+export default function getGifs({keyword = 'fail'} = {}){
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=5&offset=0&rating=g&lang=en`
+    return fetch(apiURL)
+        .then(res => res.json())
+
+        .then(response => {
+
+        const {data = []} = response
+        if (Array.isArray(data)){
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            return gifs
+        }
+    })
+}
