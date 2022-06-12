82655e87c18afa43b6a8a68d30ac241d7004ad32
Dario Acevedo <dario_acevedo_1990@hotmail.com>
2022-06-12 14:34:55 -0300

    added a spin loader

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import Gif from './Gif'
 import getGifs from '../services/getGifs'
+import './Loader.css'
 
 export default function ListOfGifs({ params }) {
 
     const { keyword } = params
+    const [loading, setLoading] = useState(false)
     const [gifs, setGifs] = useState([])
 
     useEffect(function () {
+        setLoading(true)
         getGifs({ keyword })
-        .then(gifs => setGifs(gifs))
+        .then(gifs => {
+            setGifs(gifs)
+            setLoading(false)
+        })
+
     }, [keyword])
+
+    if (loading) return <>
+                            <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                        </>
+
+
+    
 
     return <div>
         {
