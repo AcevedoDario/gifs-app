7b56da5749a595ab946cdc8a93f36f2df9f35be6
Dario Acevedo <dario_acevedo_1990@hotmail.com>
2022-06-12 14:33:43 -0300

    added three links for testing routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import ListOfGifs from "./components/ListOfGifs"
-import { Route } from "wouter"
+import { Link, Route } from "wouter"
 
 function App() {
   return (
     <div className="App">
       <h1>Gifs App</h1>
       <section className="App-content">
+        <Link to="/gif/fail">Gifs de Fails</Link>
+        <Link to="/gif/blooper">Gifs de Bloopers</Link>
+        <Link to="/gif/vine">Gifs de Vines</Link>
         <Route
           component = {ListOfGifs}
           path="/gif/:keyword"
